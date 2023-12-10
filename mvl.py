1b562ba5b2a0a5511a3e027f33f7f606d2331888
jarupxx <jarup@gmx.com>
2023-12-11 05:56:03 +0900

    フォルダー内に画像ファイルとそれ以外の混在する場合の動作を修正

--- mvl.py
+++ mvl.py
@@ -6,6 +6,7 @@ import winreg
 from ctypes import windll
 
 app_path = "C:\\path\\to\\your\\viewer.exe"
+image_extensions = ['.avif', '.bmp', '.gif', '.heic', '.jpeg', '.jpg', '.png', '.webp']
 
 def natural_sort_cmp(a, b):
     # StrCmpLogicalW 関数を使用して、自然順ソートのための比較関数を作成する
@@ -16,21 +17,22 @@ def select_file(file_path):
     try:
         folder_path = os.path.dirname(file_path)
 
-        sorted_filename = sorted(os.listdir(folder_path), key=cmp_to_key(natural_sort_cmp))
-        file_index = sorted_filename.index(os.path.basename(file_path))
+        image_files = [file for file in os.listdir(folder_path) if is_image_file(file)]
+        sorted_image_files = sorted(image_files, key=cmp_to_key(natural_sort_cmp))
+
+        file_index = sorted_image_files.index(os.path.basename(file_path))
 
         if file_index % 2 == 0:
             open_file_path = file_path
         else:
             if file_index > 0:
-                open_file_path = os.path.join(folder_path, sorted_filename[file_index - 1])
+                open_file_path = os.path.join(folder_path, sorted_image_files[file_index - 1])
 
     except Exception as e:
         print(f"エラーが発生しました: {e}")
     return open_file_path
 
 def is_image_file(file_path):
-    image_extensions = ['.avif', '.bmp', '.gif', '.heic', '.jpeg', '.jpg', '.png', '.webp']
     return any(file_path.lower().endswith(ext) for ext in image_extensions)
 
 def check_policy():
@@ -62,7 +64,7 @@ if __name__ == "__main__":
     check_policy()
 
     if not os.path.exists(app_path):
-        print("最初にmvl.pyのapp_pathをアプリのパスに書き換えてください。")
+        print("最初にmvl.pyのapp_pathとimage_extensionsをアプリのパスと対応拡張子に書き換えてください。", "\napp_path =", app_path, "\nimage_extensions =", image_extensions)
         sys.exit()
     if len(sys.argv) != 2:
         print('使用方法: mvl.py "C:\\path\\image001.jpg"')
